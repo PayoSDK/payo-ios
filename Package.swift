a450aba10074a5dffece1b808dab045ed20062f7
urrut003 <120883725+urrut003@users.noreply.github.com>
2026-02-11 19:13:24 -0800

    Update to 1.2.0

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "payo",
-            url: "https://github.com/PayoSDK/payo-ios/releases/download/1.1.0/payo.xcframework.zip",
-            checksum: "78520870b39ade4f6b9af33cb6619acb8e0ccc3b5c106bd99307aff40bd1057a"
+            url: "https://github.com/PayoSDK/payo-ios/releases/download/1.2.0/payo.xcframework.zip",
+            checksum: "522f9129b7a7df87cd5034e08eff02ea875d804e8573669c5c68998c4cb65d7e"
         ),
     ]
 )
